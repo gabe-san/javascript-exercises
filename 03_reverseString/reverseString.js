869418f6ea5d78f5b563464cf29e99980aebe6f9
gabe-san <gsantosx98@gmail.com>
2023-03-02 23:49:58 -0800

    passed all 4 tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
 
+const reverseString = function(input) {
+    let splitInput = input.split("");
+    let reverseArr = splitInput.reverse();
+    let joinArr = reverseArr.join("");
+    return joinArr
 };
+// "" in .split and .join important for removing commas and maintaing spaces between
+
+//can also do for loop(set i = str.length - 1 , then += str[i]) or recursion.
 
 // Do not edit below this line
 module.exports = reverseString;
