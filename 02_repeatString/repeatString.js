32243234399c07e1d7c2d2ea006430280fbfed7d
gabe-san <gsantosx98@gmail.com>
2023-03-02 23:00:35 -0800

    all 7 tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeated = "";
+    for (let i=0; i < num; i++) {
+        repeated += string;
+    } if (num <0) {
+        return "ERROR";
+    }
+    return repeated;
+    
 };
 
 // Do not edit below this line
