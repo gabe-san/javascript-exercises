c675fcbd5c11045b57f47d74c4632db240b6633f
gabe-san <gsantosx98@gmail.com>
2023-03-05 20:34:47 -0800

    used forEach solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
 
-};
+const removeFromArray = function(arr, ...item) {
+    item.forEach((arg) => {
+        const index = arr.indexOf(arg);
+        if (index > -1)
+        arr.splice(index, 1);
+});
+return arr;
+}
+
+//spread syntax expands parameter into an array, which lets you use array forEach
 
 // Do not edit below this line
 module.exports = removeFromArray;
