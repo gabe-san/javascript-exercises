16561253631e54d5b33377b33c63534229406692
gabe-san <gsantosx98@gmail.com>
2023-03-05 21:06:48 -0800

    used else if statements for solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = (a,b) => {
+    let finalSum = 0;
+    if (a < 0 || b < 0 || Number(a) !== a || Number(b) !==b){
+        return "ERROR"
+    } else if (a < b) {
+        for (let i = a; i <= b; i++)
+        finalSum += i;
+        return finalSum;
+    } else {
+        for(let i = b; i <= a; i++)
+        finalSum += i;
+        return finalSum;
+    }
+    }
 
-};
+
 
 // Do not edit below this line
 module.exports = sumAll;
