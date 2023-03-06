e1b1ffa06c881211e33efe2a0efcbd64baf9ea10
gabe-san <gsantosx98@gmail.com>
2023-03-05 21:30:29 -0800

    used multiple else ifs for solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+const leapYears = (year) => {
+    let x = year%4
+    let y = year%100
+    let z = year%400
 
-};
+    if (y === 0 && z === 0) {
+        return true;
+    } else if (y===0) {
+        return false;
+    } else if (x === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // Do not edit below this line
 module.exports = leapYears;
