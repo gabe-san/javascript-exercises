75150f1733ec4d17fa7fb943d484d07c9813c8e2
gabe-san <gsantosx98@gmail.com>
2023-03-05 21:42:12 -0800

    used math.round to convert 1 decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(a) {
+  let b = (a -32) * (5/9);
+  return (Math.round(b * 10) / 10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let d = ((c * (9/5) + 32));
+  return (Math.round(d* 10) / 10);
 };
 
 // Do not edit below this line
